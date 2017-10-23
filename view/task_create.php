e683a1febafc031c66a7e860de1154e9f2413614
Lehmann Jonas <blehmj@bbc-be.local>
2017-10-23 13:35:39 +0200

    add task_create.php, edit TaskController.php, TaskRepository.php and task_index.php

---
+++ view/task_create.php
@@ -0,0 +1,28 @@
+<form class="form-horizontal" action="/task/doCreate" method="post">
+	<div class="component" data-html="true">
+		<div class="form-group">
+		  <label class="col-md-2 control-label" for="title">Title</label>
+		  <div class="col-md-4">
+		  	<input id="title" name="title" type="text" placeholder="Title" class="form-control input-md">
+		  </div>
+		</div>
+		<div class="form-group">
+		  <label class="col-md-2 control-label" for="description">Description</label>
+		  <div class="col-md-4">
+		  	<input id="description" name="description" type="text" placeholder="Description" class="form-control input-md">
+		  </div>
+		</div>
+		<div class="form-group">
+		  <label class="col-md-2 control-label" for="due_date">Due Date</label>
+		  <div class="col-md-4">
+		  	<input id="due_date" name="due_date" type="date" class="form-control input-md">
+		  </div>
+		</div>
+		<div class="form-group">
+	      <label class="col-md-2 control-label" for="send">&nbsp;</label>
+		  <div class="col-md-4">
+		    <input id="send" name="send" type="submit" class="btn btn-primary">
+		  </div>
+		</div>
+	</div>
+</form>
