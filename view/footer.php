e1bb3dbd00a399d7edc64a4075e9d24ae2711f9f
Doyon Mirjam <bdoyom@bbc-be.local>
2017-10-24 12:32:00 +0200

    style more of header and content files

--- view/footer.php
+++ view/footer.php
@@ -1,7 +1,6 @@
 </div>
+    <div class="footer_push">
         <footer>
-            <div class="container">
-
                 <div class="copyr">
                     <p>©Created by Mirjam Doyon and Jonas Lehmann</p>
                 </div>
@@ -9,9 +8,8 @@
                     <p>Impressum</p>
                     <p>Contact</p>
                 </div>
-            </div>
         </footer>
-
+    </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
